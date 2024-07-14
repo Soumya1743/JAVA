bc3a8df82d95e817974f232c5f3a7acdcd728e37
Soumya1743 <soumyarangaihagari043@gmail.com>
2024-07-14 16:57:22 +0530

    JAVA Assignment

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class PerfectNumber {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
         int num = sc.nextInt();
         int sum = 0;
         for(int i=1; i<=num/2; i++){
